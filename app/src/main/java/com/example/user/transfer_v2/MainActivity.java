48894af73b3135101435240fb16649f538053311
Asus <aquabuz@gmail.com>
2017-04-28 21:18:52 +0900

    집이닷

--- app/src/main/java/com/example/user/transfer_v2/MainActivity.java
+++ app/src/main/java/com/example/user/transfer_v2/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
         }
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
