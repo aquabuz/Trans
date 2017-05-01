1097a6714e45a18d47b1b924ff9920a6ec08d356
east <aquabuz@gmail.com>
2017-05-01 18:12:57 +0900

    작업

--- app/src/main/java/com/example/user/transfer_v2/MainActivity.java
+++ app/src/main/java/com/example/user/transfer_v2/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
