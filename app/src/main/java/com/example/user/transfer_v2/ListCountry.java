84974b2dbeb546670feea617dca02d95fb6ed1b8
east <aquabuz@gmail.com>
2017-05-11 11:09:09 +0900

    0511

---
+++ app/src/main/java/com/example/user/transfer_v2/ListCountry.java
@@ -0,0 +1,19 @@
+package com.example.user.transfer_v2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 2017-05-10.
+ */
+
+public class ListCountry extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_country);
+
+
+    }
+}
