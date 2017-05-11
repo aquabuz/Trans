a9d99e3ec91cd5af392bc1539098316cc64d2087
east <aquabuz@gmail.com>
2017-05-11 19:03:50 +0900

    0511

--- app/src/main/java/com/example/user/transfer_v2/MainActivity.java
+++ app/src/main/java/com/example/user/transfer_v2/MainActivity.java
@@ -5,8 +5,15 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +55,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        textView = (TextView)findViewById(R.id.textView); // 결과를 보여줄 TextView
+        editText = (EditText)findViewById(R.id.inputText); // 값을 입력할 EditText
+
+        editText.addTextChangedListener(textWatcher); // TextWatcher 리스너 등록
+
     }
+
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable edit) {
+            // Text가 바뀌고 동작할 코드
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // Text가 바뀌기 전 동작할 코드
+        }
+
+        //
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // 입력받은 값에 100을 곱한다
+            int changeValue = Integer.parseInt(editText.getText().toString()) * 100;
+            textView.setText(changeValue+""); // 위에서 얻은 변경된값을 textView에 표시한다
+        }
+    };
 }
